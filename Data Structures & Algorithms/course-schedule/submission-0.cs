6d8447606c9e180bbe09f8251709467d8e2d7f15
Kristian Ziu <102052911+ziukristian@users.noreply.github.com>
2026-04-08 16:23:02 +0200

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cs
@@ -0,0 +1,47 @@
+public class Solution {
+    public bool CanFinish(int numCourses, int[][] prerequisites) {
+
+        var map = new Dictionary<int,List<int>>();
+
+        foreach(var p in prerequisites){
+            if(map.TryGetValue(p[0], out var pre)){
+                pre.Add(p[1]);
+            } else {
+                map[p[0]] = new List<int>(){p[1]};
+            }
+        }
+
+        var visited = new int[numCourses];
+
+        for(var i=0;i<numCourses;i++){
+            if(!canTake(i)) return false;
+        }
+
+        return true;
+
+        bool canTake(int c){
+
+            if(visited[c] == 1) return false;
+            if(visited[c] == 2) return true;
+
+            if(!map.ContainsKey(c)) {
+                
+                return true;
+            }
+
+            var pre = map[c];
+
+            visited[c] = 1;
+
+            foreach(var p in pre){
+                if(!canTake(p)) return false;
+            }
+
+            visited[c] = 2;
+
+            return true;
+
+        }
+        
+    }
+}
