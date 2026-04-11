87c20a0b990edc2185e65211297c344cf72627b7
Kristian Ziu <102052911+ziukristian@users.noreply.github.com>
2026-04-11 16:25:17 +0200

    Add: combination-target-sum - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum/submission-3.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public List<List<int>> CombinationSum(int[] nums, int target) {
+
+        var sol = new List<List<int>>();
+
+        Array.Sort(nums);
+
+        void backtrack(List<int> current, int start, int sum){
+
+            if(sum == target) {
+                sol.Add(new List<int>(current));
+                return;
+            }
+
+            if(sum > target) return;
+
+
+            for(var i = start;i<nums.Length;i++){
+
+                if(i>start && nums[i] == nums[i-1]) continue;
+
+                current.Add(nums[i]);
+                backtrack(current, i, sum+nums[i]);
+                current.RemoveAt(current.Count-1);
+            }
+
+        }
+
+        backtrack(new List<int>(),0,0);
+
+        return sol;
+
+
+        
+    }
+}
